b3c5dbe419b073932aef05f15411d15fbdaf97c0
duy366110 <duy366110@gmail.com>
2024-01-10 16:28:23 +0700

    #Commit -m reponsive carousel success.

--- src/components/pages/Page-Home-Component/Home-Card-Category-Component/Home-Card-Category-Component.jsx
+++ src/components/pages/Page-Home-Component/Home-Card-Category-Component/Home-Card-Category-Component.jsx
@@ -1,11 +1,18 @@
+import { NavLink } from "react-router-dom";
 import classes from "./Home-Card-Category-Component.module.css";
 
 const HomeCardCategoryComponent = (props) => {
 
     return (
-        <div className={classes['home-category-component']}>
+        <NavLink to="/" className={classes['home-category-component']}>
             <img src={props.category.images[0]} alt="" />
-        </div>
+            <div className={classes['home-category-infor']}>
+                <h2 className={classes['title']}>{props.category.title}</h2>
+                <h3 className={classes['sub-title']}>
+                    Số khách sạn {props.category.collections.length} hiện có
+                </h3>
+            </div>
+        </NavLink>
     )
 }
 
